6847da5f7164e70c6f5307066eed53b772ef8d3a
Roshan Shaik <rshaik@ariqt.com>
2021-12-29 11:55:42 +0530

    initial commit

---
+++ apps/tuskdesk/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fsa-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'tuskdesk';
+}
